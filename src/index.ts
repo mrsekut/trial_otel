82f4a05c7179401bc0cb0a749c289a90a6d6ebf9
mrsekut <k.cloudspider@gmail.com>
2025-07-05 17:01:40 +0900

    add rolldice endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 await import('./instrumentation.js');
 import { otel } from '@hono/otel';
 import { Hono } from 'hono';
+import { rollTheDice } from './rolldice.js';
 
 const app = new Hono();
 
 app.use('*', otel());
 app.get('/', c => c.text('foo'));
+
+app.get('/rolldice', c => {
+  // λ curl http://localhost:3000/rolldice?rolls=12
+  const qRolls = c.req.query('rolls');
+  const rolls = qRolls == null ? 1 : parseInt(qRolls.toString());
+  return c.json(rollTheDice(rolls, 1, 6));
+});
 
 export default app;
---
+++ src/rolldice.ts
@@ -0,0 +1,12 @@
+export function rollTheDice(rolls: number, min: number, max: number) {
+  const result: number[] = [];
+  for (let i = 0; i < rolls; i++) {
+    result.push(rollOnce(min, max));
+  }
+
+  return result;
+}
+
+function rollOnce(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
